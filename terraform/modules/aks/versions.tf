ce6596a83d734417ef9b49f77cfe921fb784a299
Birol Tilki <btilki@gmail.com>
2026-07-15 00:11:39 +0200

    fix: pre-commit scope for Terraform validate and vendored Helm charts

---
+++ terraform/modules/aks/versions.tf
@@ -0,0 +1,9 @@
+# Provider pin for standalone `terraform validate` (pre-commit) in this module directory.
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.100"
+    }
+  }
+}
